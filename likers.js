6f6c3ab59bee6d2210ddfef4bfa28f53300e1efa
boblemarin <emeric@minimal.be>
2014-02-18 10:53:52 +0100

    initial commit

---
+++ likers.js
@@ -0,0 +1,58 @@
+function addLikers(container){
+  var likers = container.querySelectorAll('.liker');
+
+  Array.prototype.forEach.call(likers, function(el, i) {
+
+    var value = Number(el.dataset.value) || 0;
+
+    var likeBtn = document.createElement('span');
+    likeBtn.className = 'liker-like-btn';
+    likeBtn.innerText = 'LIKE';
+    likeBtn.addEventListener('click', function(e){
+      if ( value === 1 ) {
+        value = 0;
+      } else {
+        value = 1;
+      }
+      updateElements();
+    });
+
+    var dislikeBtn = document.createElement('span');
+    dislikeBtn.className = 'liker-like-btn';
+    dislikeBtn.innerText = 'DISLIKE';
+    dislikeBtn.addEventListener('click', function(e){
+      if ( value === -1 ) {
+        value = 0;
+      } else {
+        value = -1;
+      }
+      updateElements();
+    });
+
+    function updateElements(){
+      switch( value ) {
+        case 0:
+          likeBtn.classList.remove("selected");
+          dislikeBtn.classList.remove("selected");
+          break;
+        case 1:
+          likeBtn.classList.add("selected");
+          dislikeBtn.classList.remove("selected");
+          break;
+        case -1:
+          likeBtn.classList.remove("selected");
+          dislikeBtn.classList.add("selected");
+          break;
+      }
+
+      el.dataset.value = value;
+    }
+
+    el.appendChild( likeBtn );
+    el.appendChild( dislikeBtn );
+
+    updateElements();
+
+  });
+
+}
